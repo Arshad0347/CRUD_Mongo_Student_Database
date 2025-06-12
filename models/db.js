5fee9898e889c09b64683b17afd2b3c958f23cb7
Arshad0347 <arshad.eqbal009@gmail.com>
2025-06-12 14:10:44 +0530

    updates

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     await mongoose.connect(
-      'mongodb+srv://arshadeqbal:1234567890@cluster1.vuy03t3.mongodb.net/students?retryWrites=true&w=majority&appName=Cluster1',
+      process.env.MONGO_URL
     );
     console.log(`MongoDB Connected Successfully... ${connectDB}` );
   } catch (err) {
